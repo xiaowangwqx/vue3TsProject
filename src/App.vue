dbf3fc6c4768c3800ab885456f847e4651c0d799
wqx <wqx135@foxmail.com>
2023-12-13 14:59:15 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>你好</h1>
+  <div class="box">123465</div>
+</template>
+<script>
+import vue from 'vue';
+</script>
+<style scoped lang="scss">
+.box {
+  width: 100px;
+  height: 100px;
+  background: pink;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
+
+const fun=()=>{
+    console.log("没有格式化的代码");
+}
+fun();
