cc06aa64d7bec903cbdbf7f1a3feb3ef3f6731c1
wqx <wqx135@foxmail.com>
2023-12-13 15:51:24 +0800

    chore: 配置commitlint

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,7 @@
   <h1>你好</h1>
   <div class="box">123465</div>
 </template>
-<script setup lang="ts">
-for (var i = 0; i < 10; i++) {
-  console.log(i, "123456");
-}
-</script>
+<script setup lang="ts"></script>
 <style scoped lang="scss">
 .box {
   width: 100px;
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,3 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 createApp(App).mount('#app');
-
-const fun = () => {
-  console.log('没有格式化的代码');
-  var age=10;
-};
-fun();
