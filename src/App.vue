fdff1b9b78bfcaea1b66d0ad99aeb14e7408664c
wqx <wqx135@foxmail.com>
2023-12-14 09:49:28 +0800

    chore: 配置提交规范，强制使用pnpm 包管理工具

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
   <h1>你好</h1>
   <div class="box">123465</div>
+  <el-button>123</el-button>
+  <el-button type="primary">Primary</el-button>
+  <el-button type="primary" :icon="Edit">Edit</el-button>
 </template>
-<script setup lang="ts"></script>
+
+<script setup lang="ts">
+import { Edit } from '@element-plus/icons-vue';
+</script>
 <style scoped lang="scss">
 .box {
   width: 100px;
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
 import { createApp } from 'vue';
-import App from './App.vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+//@ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
+// import App from './App.vue';
+import App from '@/App.vue';
 
-createApp(App).mount('#app');
+// 获取应用实例对象
+const app = createApp(App);
+app.use(ElementPlus);
+app.use(ElementPlus, {
+  locale: zhCn,
+});
+
+// 将应用挂载到挂载点上
+app.mount('#app');
